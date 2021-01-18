68f100dac53f13d20e64259050b0795d54023f15
MohammedM <nanumiah@nanus-mbp.home>
2021-01-18 10:22:52 +0000

    Updates

---
+++ precache-manifest.eab53e091004828ad6e1f77f5248a24a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d96bda8172c0bf335e9b0985c2facae",
+    "url": "/react_reviews/index.html"
+  },
+  {
+    "revision": "1e976dac589c77d875c8",
+    "url": "/react_reviews/static/css/main.3d9f8e8d.chunk.css"
+  },
+  {
+    "revision": "9cf77318e524bbf5d860",
+    "url": "/react_reviews/static/js/2.aab8ba5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_reviews/static/js/2.aab8ba5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e976dac589c77d875c8",
+    "url": "/react_reviews/static/js/main.94b8db10.chunk.js"
+  },
+  {
+    "revision": "ed885ed06716640c2686",
+    "url": "/react_reviews/static/js/runtime-main.9c7472e2.js"
+  }
+]);
